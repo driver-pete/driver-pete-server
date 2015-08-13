41cea97c599fb566e402602f3f8d742db4b09066
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-13 07:41:27 -0700

    add context configuration back

--- src/test/java/com/otognan/driverpete/logic/TrajectoryReaderTest.java
+++ src/test/java/com/otognan/driverpete/logic/TrajectoryReaderTest.java
@@ -28,22 +28,24 @@ import com.amazonaws.services.s3.model.S3Object;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes={AWSConfiguration.class})
+@ContextConfiguration(classes={AWSConfiguration.class,
+        TrajectoryReaderTest.TestPropertiesConfiguration.class
+})
 public class TrajectoryReaderTest {
     
     @Autowired
     AWSCredentials awsCredentials;
 
-//    @Configuration
-//    static class TestPropertiesConfiguration {
-//        @Bean
-//        PropertyPlaceholderConfigurer propConfig() {
-//            PropertyPlaceholderConfigurer ppc =  new PropertyPlaceholderConfigurer();
-//            ppc.setLocation(new ClassPathResource("security.properties"));
-//            ppc.setIgnoreResourceNotFound(true);
-//            return ppc;
-//        }
-//    }
+    @Configuration
+    static class TestPropertiesConfiguration {
+        @Bean
+        PropertyPlaceholderConfigurer propConfig() {
+            PropertyPlaceholderConfigurer ppc =  new PropertyPlaceholderConfigurer();
+            ppc.setLocation(new ClassPathResource("security.properties"));
+            ppc.setIgnoreResourceNotFound(true);
+            return ppc;
+        }
+    }
 
     @Test
     public void testTrajectoryReader() throws IOException, ParseException {

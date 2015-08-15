14be57c12fbb0ed987b79561ff4018bace31e5af
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-15 13:08:48 -0700

    logic draft..

--- src/main/java/com/otognan/driverpete/logic/TrajectoryEndpointWorker.java
+++ src/main/java/com/otognan/driverpete/logic/TrajectoryEndpointWorker.java
@@ -19,4 +19,34 @@ public class TrajectoryEndpointWorker {
             }
         }
     }
+    
+    @Async
+    public void findEndpoints(String trajectoryKey) {
+        
+        //download
+        //read 
+        
+        // create filters
+        DuplicateTimeFilter duplicateTime = new DuplicateTimeFilter();
+        StationaryPointsFilter stationaryPoint = new StationaryPointsFilter();
+        VelocityOutliersFilter velocityOutlier = new VelocityOutliersFilter(85.);
+        FindEndpointsProcessor processor = new FindEndpointsProcessor();
+        
+        // get user state for endpoints
+        // put state into filters
+        
+        // do filtering
+        // get endpoitns
+        
+        // determine if need to upload endpoints
+        
+        // AS A TRANSACTION
+        // read state and ednpoints, determine that the same as before,
+        // determine that trajectory key still exists
+        // upload endpoints and new state
+        // if successful, 
+        // remove trajectoryKey (or copy to "processed")
+        // FINISH TRANSACTION
+        
+    }
 }

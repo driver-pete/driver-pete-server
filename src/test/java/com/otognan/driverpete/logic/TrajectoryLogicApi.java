f96f13e52948396b86754109de8a7207d8a4e9e8
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-18 08:21:48 -0700

    public

--- src/test/java/com/otognan/driverpete/logic/TrajectoryLogicApi.java
+++ src/test/java/com/otognan/driverpete/logic/TrajectoryLogicApi.java
@@ -14,12 +14,15 @@ import retrofit.mime.TypedInput;
 
 public interface TrajectoryLogicApi {
     @POST("/api/trajectory/compressed_length")
-    int compressedLength(@Body TypedInput body);
+    public int compressedLength(@Body TypedInput body);
  
     @POST("/api/trajectory/compressed")
-    Response compressed(@Query("label") String label, @Body TypedInput body);
+    public Response compressed(@Query("label") String label, @Body TypedInput body);
     
     @GET("/api/trajectory/endpoints")
-    List<TrajectoryEndpoint> trajectoryEndpoints();
+    public List<TrajectoryEndpoint> trajectoryEndpoints();
+    
+    @GET("/api/trajectory/routes")
+    public List<String> routes(@Query("isAtoB") boolean isAtoB);
 }
     

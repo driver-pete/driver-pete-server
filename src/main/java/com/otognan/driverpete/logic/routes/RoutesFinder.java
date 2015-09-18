74dc0375e1e5e6b49d4d7e6e76cc889551922c6b
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-09-17 23:28:02 -0700

    start route if continutity is broken but didn't move far

--- src/main/java/com/otognan/driverpete/logic/routes/RoutesFinder.java
+++ src/main/java/com/otognan/driverpete/logic/routes/RoutesFinder.java
@@ -66,7 +66,14 @@ public class RoutesFinder {
             double dt = Location.deltaTime(
                     this.currentRoute.get(this.currentRoute.size()-1), point);
             if (dt > this.continuityThreshold) {
-                this.stopRoute();
+                int index = this.closestEndpointIndex(point);
+                if (index == this.fromEndpointIndex) {
+                    this.stopRoute();
+                    // We didnt move too far from beginning, so we start the route there
+                    this.startRoute(point, index);
+                } else {
+                    this.stopRoute();
+                }
                 return;
             }
             
